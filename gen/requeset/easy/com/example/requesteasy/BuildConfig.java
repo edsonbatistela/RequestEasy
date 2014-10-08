ad7f5c6cfe27e667d3272e98ccdfeffcdd8c2e3f
edsonbatistela <edson_batistela13@hotmail.com>
2014-10-07 22:32:39 -0300

    first commit

---
+++ gen/requeset/easy/com/example/requesteasy/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package requeset.easy.com.example.requesteasy;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
